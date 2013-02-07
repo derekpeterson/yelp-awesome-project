228501ae10006730b04e997485278a080b0d68e1
dpeterson <derekjpeterson@gmail.com>
2013-02-06 19:39:22 -0500

    Accessing dataset function added

--- starter.py
+++ starter.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python
 
+import sys, re, json
+
+def access_dataset():
+	with open('yelpdataset.json') as yelpdataset:
+		data = json.load(yelpdataset)
+
+	for line in data:
+		print line['business_id']
+
 def main():
-	print "Project started!"
+	access_dataset()
 
 if __name__ == "__main__":
 	main()
